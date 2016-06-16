28a3e016b1f624471786daf94f3f4e9036172856
Gabriel Zhou <averyczech@hotmail.com>
2016-06-16 02:06:08 -0400

    Remove web client V0.1.5

--- Data-Service-client/javascript/config.js
+++
@@ -1,4 +0,0 @@
-var restURL = "http://localhost:9001";
-var host = "http://localhost:9002/";
-var couchDB_dataobject = "http://127.0.0.1:5984/_utils/document.html?dataobject/"
-var landingpageURI="http://localhost:9002/landingpage.html"
