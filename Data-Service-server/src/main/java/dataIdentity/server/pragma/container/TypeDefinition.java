fb624e59eaa67d68f1e28291f48b143893813ecc
Gabriel Zhou <averyczech@hotmail.com>
2016-07-21 23:10:47 +0900

    Update src with CORS support

---
+++ Data-Service-server/src/main/java/dataIdentity/server/pragma/container/TypeDefinition.java
@@ -0,0 +1,77 @@
+package dataIdentity.server.pragma.container;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * Encapsulates a type in the type registry, roughly defined as a set of
+ * properties.
+ * 
+ */
+public class TypeDefinition {
+
+	/**
+	 * PID of the type.
+	 */
+	@JsonProperty("identifier")
+	protected String identifier;
+
+	/**
+	 * Value (boolean) True means mandatory, False means optional.
+	 */
+	@JsonProperty("properties")
+	protected HashMap<String, PropertyDefinition> properties;
+
+	@JsonProperty("explanationOfUse")
+	protected String explanationOfUse;
+
+	@JsonProperty("description")
+	protected String description;
+
+	public TypeDefinition(String identifier, String explanationOfUse, String description) {
+		this.identifier = identifier;
+		this.explanationOfUse = explanationOfUse;
+		this.description = description;
+		this.properties = new HashMap<String, PropertyDefinition>();
+	}
+
+	@JsonCreator
+	public TypeDefinition(@JsonProperty("identifier") String identifier,
+			@JsonProperty("explanationOfUse") String explanationOfUse, @JsonProperty("description") String description,
+			@JsonProperty("properties") HashMap<String, PropertyDefinition> Properties) {
+		this.identifier = identifier;
+		this.explanationOfUse = explanationOfUse;
+		this.description = description;
+		this.properties = Properties;
+	}
+
+	public void addProperty(String propertyIdentifier, PropertyDefinition prop_def) {
+		this.properties.put(propertyIdentifier, prop_def);
+	}
+
+	/**
+	 * Returns a set of all properties. The caller will not be able to
+	 * distinguish between mandatory and optional properties.
+	 * 
+	 * @return a set of property identifiers (strings)
+	 */
+	@JsonIgnore
+	public Set<String> getAllProperties() {
+		return new HashSet<String>(properties.keySet());
+	}
+
+	@JsonIgnore
+	public Map<String, PropertyDefinition> getProperties() {
+		return this.properties;
+	}
+
+	public String getIdentifier() {
+		return identifier;
+	}
+}
---
+++ Data-Service-server/src/main/java/dataIdentity/server/pragma/response/MessageResponse.java
@@ -0,0 +1,36 @@
+package dataIdentity.server.pragma.response;
+
+/**
+ * 
+ * Normal message rest response
+ *
+ */
+public class MessageResponse {
+	private boolean success;
+	private String message;
+
+	public MessageResponse() {
+
+	}
+
+	public MessageResponse(boolean success, String message) {
+		this.success = success;
+		this.message = message;
+	}
+
+	public boolean isSuccess() {
+		return success;
+	}
+
+	public void setSuccess(boolean success) {
+		this.success = success;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
